c36d1743751492a66df342d51b58000e18a95424
LinFelix <felix-voelker@gmx.net>
2017-09-16 09:25:14 +0200

    wrapper done in dev

--- libs/reutherAPIwrapper.py
+++ libs/reutherAPIwrapper.py
@@ -2,9 +2,12 @@
 
 import requests
 import xml.etree.ElementTree as ET
+import os.path
 
 """
+How to use this script?
 
+look in the main part down below
 
 
 """
@@ -20,6 +23,7 @@ SERVICE_URL = "http://rmb.reuters.com/rmd/rest/json/"
 class ReutherAPIWrapper:
 
     def __init__(self, username=USERNAME, password=PASSWORD):
+        #  get an access token and store it
         _token_response = ET.fromstring(requests.get(
             '{}login?username={}&password={}'
             .format(AUTH_URL, username, password)).text)
@@ -30,6 +34,10 @@ class ReutherAPIWrapper:
         self.available_channels = {}
 
     def get_channel_list(self):  # without categories
+        """
+        gets all available channels
+        :return: a dict with {<channel-name> : <channel-alias>}
+        """
         _channel_list_response = requests.get(
             '{}/channels?&token={}'.format(SERVICE_URL_XML, self._authToken))
         _channel_list_tree_root = ET.fromstring(_channel_list_response.text)
@@ -43,37 +51,45 @@ class ReutherAPIWrapper:
 
     def get_items_meta(self, channel_alias):
         return requests.get('{}items?channel={}&token={}'.format(SERVICE_URL, channel_alias, self._authToken)).json()['results']
-        #items_meta_list =
-        #for items in items_meta.json():
-        #    items_meta_list[items] = items_meta.json()[items]
-        #return items_meta_list
 
     def get_item_content(self, item):
         return requests.get('{}item?id={}&token={}'.format(SERVICE_URL, item['id'], self._authToken)).json()
 
-    def throw_out_link_list(self, mylist):
-        return list(filter(lambda x: x['headline'] != 'OUSWDM Link List', mylist))
+    def get_items_meta_without_link_list(self, channel_alias):
+        return list(filter(lambda x: x['headline'] != 'OUSWDM Link List', self.get_items_meta(channel_alias)))
 
     def has_text(self, item):
         return item['productlabel'] == 'text'
 
+    def get_channel_aliasis(self):
+        return list(self.get_channel_list().values())
+
+
+def save_all_text(new_path):
+    if os.path.exists(new_path):
+        print("not going to overwrite files")
+    else:
+        os.mkdir(new_path)
+        count = 0
+        raw = ReutherAPIWrapper("HackZurichAPI", "8XtQb447")  # instantiate and gain access token
+        channels = raw.get_channel_aliasis()  # get a list of channels
+        for channel in channels:
+            items_list = raw.get_items_meta_without_link_list(channel)
+            for items in items_list:
+                content = raw.get_item_content(items)
+                if raw.has_text(content):
+                    text = content["body_xhtml"]
+                    with open(new_path + '/' + str(count) + ".txt", 'w') as f:
+                        f.write(text)
+                    count += 1
+
+
 if __name__ == '__main__':
-    reutherAPIWrapper = ReutherAPIWrapper("HackZurichAPI", "8XtQb447")
-    channel_list = reutherAPIWrapper.get_channel_list()
-    items_list = reutherAPIWrapper.throw_out_link_list(reutherAPIWrapper.get_items_meta(list(channel_list.values())[0]))
-    for i in range(200):
-        content = reutherAPIWrapper.get_item_content(items_list[i])
-        if reutherAPIWrapper.has_text(content):
-            print(content['body_xhtml'])
-    # printing headlines
-    #for i in items_list:
-    #    print(i['headline'])
-
-
-
-    #for i in items_list:
-    #    print(i.headline)
-    #    if i.headline == 'en':
-    #        reutherAPIWrapper.get_item_content(i)
-
-    #reutherAPIWrapper.get_item_content(items_list[0])
+    raw = ReutherAPIWrapper("HackZurichAPI", "8XtQb447")  # instantiate and gain access token
+    channels = raw.get_channel_aliasis()  # get a list of channels
+    for channel in channels:
+        items_list = raw.get_items_meta_without_link_list(channel)
+        for items in items_list:
+            content = raw.get_item_content(items)
+            if raw.has_text(content):
+                print(content["body_xhtml"])
