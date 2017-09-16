e5f1216583c8848926cfb1e9b4ac05c06a0b2c76
LinFelix <felix-voelker@gmx.net>
2017-09-17 00:24:35 +0200

    using list of tags instead of channels

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 # /bin/env python3
 
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, render_template
 from libs import reutherAPIwrapper as rw
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    raw = rw.ReutherAPIWrapper("HackZurichAPI", "8XtQb447")
-    return render_template('index.html', channels=raw.get_channel_list())
+    # raw = rw.ReutherAPIWrapper("HackZurichAPI", "8XtQb447")
+    return render_template('index.html', channels=
+    ['Disaster_Accident', 'Environment', 'Health_Medical_Pharma', 'Education', 'Business_Finance', 'Politics',
+     'Social Issues', 'War_Conflict', 'Entertainment_Culture', 'Law_Crime', 'Human Interest', 'Sports',
+     'Technology_Internet', 'Religion_Belief', 'Labor', 'Weather', 'Hospitality_Recreation'])
 
-@app.route('/',methods=['POST'])
+
+@app.route('/', methods=['POST'])
 def post_choosen_channel_alias():
     print("blubb")
-    print(request.json['alias'])
+    list_of_chosen_tags = request.json['alias']
+    pass_list_of_tags(list_of_chosen_tags)
     return "Hello world"
-
 
 
 def pass_list_of_tags(list_of_tags):
