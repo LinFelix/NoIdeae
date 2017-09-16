c68647831cd22dd62f15c968c70816d04e7a8dba
LinFelix <felix-voelker@gmx.net>
2017-09-16 22:02:10 +0200

    working prototype of guy

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ app = Flask(__name__)
 def hello_world():
     raw = rw.ReutherAPIWrapper("HackZurichAPI", "8XtQb447")
     return render_template('index.html', channels=raw.get_channel_list())
+
+@app.route('/',methods=['POST'])
+def post_choosen_channel_alias():
+    print("blubb")
+    print(request.json['alias'])
+    return "Hello world"
