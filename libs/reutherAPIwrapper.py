a546af5b76ec491efe1d4a6ab4c511a8db8ced3f
LinFelix <felix-voelker@gmx.net>
2017-09-16 06:48:54 +0200

    reutherAPIwrapper collect meta data of items

--- libs/reutherAPIwrapper.py
+++ libs/reutherAPIwrapper.py
@@ -10,6 +10,19 @@ AUTH_URL = "https://commerce.reuters.com/rmd/rest/xml/"
 SERVICE_URL = "http://rmb.reuters.com/rmd/rest/xml/"
 
 
+class MetaItem:
+
+    def __init__(self, content):
+        self.id = content[0].text
+        self.version = content[2].text
+        self.dateCreated = content[3].text
+        self.source = content[5].text
+        self.language = content[6].text
+        self.headline = content[7].text
+        self.mediatype = content[8].text
+        self.channel = content[10].text
+
+
 class ReutherAPIWrapper:
 
     def __init__(self, username=USERNAME, password=PASSWORD):
@@ -31,18 +44,24 @@ class ReutherAPIWrapper:
                 self.available_channels[channel_information[0].text] =\
                     channel_information[1].text
             except:
-                # print(channel_information.text)
                 pass  # channel_information faulty
+        return self.available_channels
 
     def get_items_meta(self, channel_alias):
         _items_respons =\
             requests.get('{}/items?channel={}&token={}'
                          .format(SERVICE_URL, channel_alias, self._authToken))
-        itemslist = []
+        self.itemslist = []
         for result in ET.fromstring(_items_respons.text):
-            pass
+            try:
+                self.itemslist.append(MetaItem(result))
+            except:
+                pass  # bad xml
+        return self.itemslist
 
 
 if __name__ == '__main__':
     reutherAPIWrapper = ReutherAPIWrapper("HackZurichAPI", "8XtQb447")
-    reutherAPIWrapper.get_channel_list()
+    channel_list = reutherAPIWrapper.get_channel_list()
+    for i in reutherAPIWrapper.get_items_meta(list(channel_list.values())[0]):
+        print(i.headline)
